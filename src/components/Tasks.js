eeaac21682289213a669e400faa0054cdd6fcffd
P M <przemyslaw.musialik@soprasteria.com>
2019-05-31 16:19:10 +0200

    Extract data

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import Box from './Box';
 import './Tasks.scss';
+import { tasks } from '../data';
 
 function onMake(event, id) {
   event.target.closest('.Box').classList.add("erasing")
 }
-
-const tasks = [
-  {
-    id: 1,
-    title: 'Make a coffee',
-    content: 'Maybe we should confider making stronger coffee with less milk so we could be awake earlier and absorb less fat and calories',
-  },
-  {
-    id: 2,
-    title: 'Run Runmageddon',
-    content: 'We would like to participate in Runmageddon Silesia wit Sopra Steria team. https://www.runmageddon.pl/imprezy/runmageddon-silesia-jsw-s-a-31-08-2019',
-    date: '31 August (in 3 months)',
-    place: 'Górnicza 1, 44-193 Knurów',
-  },
-  {
-    id: 3,
-    title: 'Play match',
-    content: null,
-    date: '17:00, 4 June (in 4 days)',
-    place: 'Będzin',
-  },
-  {
-    id: 4,
-    title: 'Buy some things to put on the balcony',
-  },
-  {
-    id: 5,
-    title: 'Buy some things for dinner',
-    content: null,
-    date: '31.05 (today)',
-  },
-]
 
 function Tasks() {
   return (
