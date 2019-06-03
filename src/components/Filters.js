3566da08c7b502ada2fd0768e28b48e01c9666ec
P M <przemyslaw.musialik@soprasteria.com>
2019-06-03 16:02:35 +0200

    Update filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './Filters.scss';
 
+function onSelectFilter(event) {
+  const $filtersBody = event.target.closest('.Filters__body');
+  const $filter = event.target;
+
+  $filtersBody.querySelectorAll('.selected').forEach(element => {
+    element.classList.remove('selected');
+  });
+
+  $filter.classList.add('selected');
+}
+
 function Filters() {
   return (
     <div className="Filters">
-      <div>Show tasks for today</div>
-      <div>Show tasks for tomorrow</div>
-      <div>Show tasks for next 3 days</div>
-      <div>Show tasks without date</div>
-      <div>Filter by deadline </div>
+      <div className="Filters__header">
+        <input type="text" placeholder="Filter..."></input>
+      </div>
+      <div className="Filters__body">
+        <div className="Filters__filter" onClick={onSelectFilter}>Show tasks for today</div>
+        <div className="Filters__filter" onClick={onSelectFilter}>Show tasks for tomorrow</div>
+        <div className="Filters__filter" onClick={onSelectFilter}>Show tasks for next 3 days</div>
+        <div className="Filters__filter" onClick={onSelectFilter}>Show tasks without date</div>
+        <div className="Filters__filter" onClick={onSelectFilter}>Filter by deadline </div>
+      </div>
     </div>
   );
 }
