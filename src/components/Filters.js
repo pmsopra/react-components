4017b33d0974baee47104323579b90735eb1a58f
P M <przemyslaw.musialik@soprasteria.com>
2019-06-03 14:14:36 +0200

    Fix filters position

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,7 +4,11 @@ import './Filters.scss';
 function Filters() {
   return (
     <div className="Filters">
-      Filters...
+      <div>Show tasks for today</div>
+      <div>Show tasks for tomorrow</div>
+      <div>Show tasks for next 3 days</div>
+      <div>Show tasks without date</div>
+      <div>Filter by deadline </div>
     </div>
   );
 }
