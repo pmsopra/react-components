3b568ad6911439cf166dab91f42a82a1b24098aa
P M <przemyslaw.musialik@soprasteria.com>
2019-06-03 10:25:50 +0200

    Init navbar, add global scss variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Tasks from './components/Tasks';
+import Navbar from './components/Navbar';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Tasks />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.scss';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="Navbar__left">
+        System
+      </div>
+
+      <div className="Navbar__search">
+
+      </div>
+
+      <div className="Navbar__right">
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
